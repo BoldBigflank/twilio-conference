809c5d2af3ed380d04a227f5ce91473bc1779829
Ash <ayeshigeta@github.com>
2013-08-02 00:49:11 +0000

    removed newline characters

--- welcome.php
+++ welcome.php
@@ -3,8 +3,8 @@
 ?>
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
-	<Gather action = './process-pin.php' method='GET' numDigits='6'>\n
-		<Say>Please enter your 6-digit pin</Say>\n
+	<Gather action = './process-pin.php' method='GET' numDigits='6'>
+		<Say>Please enter your 6-digit pin</Say>
 	</Gather>
 	<Redirect>#</Redirect>
 </Response>
