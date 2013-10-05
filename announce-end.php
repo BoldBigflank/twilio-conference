dfee1d08c94fca793cb428da45805c291bfa5528
Alex <alex@bold-it.com>
2013-10-05 20:44:48 +0000

    no recording fix

--- announce-end.php
+++ announce-end.php
@@ -6,7 +6,7 @@ $response = '<?xml version="1.0" encoding="UTF-8"?><Response>';
 $RecordingUrl = isset($_GET['RecordingUrl']) ? $_GET['RecordingUrl'] : "''";
 
 
-if($RecordingUrl != "" ){
+if($RecordingUrl != "''" ){
 	$response .= "<Play>$RecordingUrl</Play>";
 	$response .= '<Say voice="alice" language="en-GB">has left the conference.</Say>';
 
